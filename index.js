68133c8d6b459c0ea0aaee4f8cf3a9666e0b879e
ShivaGanji57 <shivakumarganjiatme@gmail.com>
2023-08-12 21:33:42 +0530

    added delete and edit button done functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var form=document.getElementById('addForm')
+//var edit=document.getElementById("edit")
 form.addEventListener('submit',storeLocal)
-form.addEventListener('click',deleteFromLocal)
+//form.addEventListener('click',deleteFromLocal)
+//edit.addEventListener('click',editLocal)
 function storeLocal(e){
     e.preventDefault();
     //create object
@@ -12,10 +14,28 @@ function storeLocal(e){
     //store as an object, email is unique so will use email as key
     let myobj_serilized=JSON.stringify(myobj)
     localStorage.setItem(document.getElementById('email').value,myobj_serilized)
+    showUseronScreen(myobj)
 }
-//delete from local using key
-function deleteFromLocal(e){
-    e.preventDefault();
-    var keyemail=document.getElementById('deleteKey').value
-    localStorage.removeItem(keyemail)
+function showUseronScreen(obj){
+    const parentElem=document.getElementById('list-items')
+    var li=document.createElement('li')
+    li.textContent=obj.userName+' - '+obj.userEmail+' - '+obj.userNumber;
+    var deletebtn=document.createElement('input')
+    deletebtn.type='button'
+    deletebtn.value='delete'
+    var editbtn=document.createElement('input')
+    editbtn.type='button'
+    editbtn.value='edit'
+    editbtn.onclick=() =>{
+        localStorage.removeItem(obj.userEmail)
+        parentElem.removeChild(li)
+        li.textContent=obj.userName+' - '+obj.userEmail+' - '+obj.userNumber;
+    }
+    deletebtn.onclick=() =>{
+        localStorage.removeItem(obj.userEmail)
+        parentElem.removeChild(li)
+    }
+    li.appendChild(deletebtn)
+    li.appendChild(editbtn)
+    parentElem.appendChild(li)
 }
