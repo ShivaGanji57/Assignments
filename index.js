ecafb4ab50436a0c19d64bb52871f3de9340858f
ShivaGanji57 <shivakumarganjiatme@gmail.com>
2023-08-12 14:00:55 +0530

    store in local

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var form=document.getElementById('addForm')
+form.addEventListener('submit',storeLocal)
 
+function storeLocal(e){
+    e.preventDefault();
+    localStorage.setItem(document.getElementById('email').value,document.getElementById('name').value)
+}
