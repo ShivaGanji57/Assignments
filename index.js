ed9f8fd59e2370178f469155903af8458893ea42
ShivaGanji57 <shivakumarganjiatme@gmail.com>
2023-08-12 17:50:00 +0530

    store in local as objects

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ form.addEventListener('submit',storeLocal)
 
 function storeLocal(e){
     e.preventDefault();
-    localStorage.setItem(document.getElementById('email').value,document.getElementById('name').value)
+    //create object
+    let myobj={
+        userName:document.getElementById('name').value,
+        userEmail: document.getElementById('email').value,
+        userNumber:document.getElementById('Mobile').value
+    };
+    //store as an object
+    let myobj_serilized=JSON.stringify(myobj)
+    localStorage.setItem('myobj',myobj_serilized)
 }
